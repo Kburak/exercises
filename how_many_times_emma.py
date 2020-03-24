cb3e7d5cfc7ce30d336ee1dcd4e8f6b3d2f065a1
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-24 14:54:03 +0300

    Create how_many_times_emma.py

---
+++ how_many_times_emma.py
@@ -0,0 +1,9 @@
+def how_many_times(text):
+    count = 0
+    for word in range(len(text)-1):
+        count += text[word:word+4] ==  "Emma"
+    return count
+my_string = "Emma is a good developer. Emma is also a writer"
+count = how_many_times(my_string)
+print("Emma appeared", count, "times")
+
