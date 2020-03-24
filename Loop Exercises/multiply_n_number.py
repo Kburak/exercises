52c1bede10b1c1bf138ac0dcf7c16886c458ceec
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-24 16:15:56 +0300

    loop_exercises

---
+++ Loop Exercises/multiply_n_number.py
@@ -0,0 +1,4 @@
+number = int(input("Enter a number"))
+for num in range(1,11):
+    multiply = number*num
+    print(multiply)
---
+++ Loop Exercises/sum_n_number.py
@@ -0,0 +1,7 @@
+sum = 0
+
+number = int(input("Enter a number"))
+
+for num in range(1,number+1):
+    sum += num
+print(sum)
