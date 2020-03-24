3ddfa228763d11468e266516bad7e61872e29a6b
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-24 14:59:34 +0300

    Delete remove_number_of_chars.py

--- remove_number_of_chars.py
+++
@@ -1,9 +0,0 @@
-def remove_chars(text,num):
-    if len(text)>num:
-        return text[num:]
-    else:
-        return "Number should be less than the length"
-
-print("Removing n number of chars")
-print(remove_chars("pynative",4))
-#number must be less than the length of string
