716256c3e697daa7198bcfcfd3f78c53b7e9274d
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-24 14:59:31 +0300

    Delete first_last.py

--- first_last.py
+++
@@ -1,8 +0,0 @@
-def is_same(num):
-    if numlist[0] == numlist[-1]:
-        return True
-    else:
-        return False
-numlist = [1,2,3,4,5,1]
-print("the first and the last number of a list is same")
-print("result is", is_same(numlist))
