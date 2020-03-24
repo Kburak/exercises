1c129f628460bfa6f817aa0dd556ff37b9171f1b
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-24 14:59:30 +0300

    Delete even_index.py

--- even_index.py
+++
@@ -1,8 +0,0 @@
-def even_func(text):
-    for char in range(0,len(text)-1,2):
-        print("index[char]",text[char])
-
-my_string = input("Enter String")
-print("Original string is ", my_string)
-print("Printing only even index chars")
-even_func(my_string)
