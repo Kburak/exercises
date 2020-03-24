e4a05985f2799542665cc77b43859c0e564b6778
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-24 15:42:02 +0300

    updated an error (index did not appear)

--- Basic Exercises/even_index.py
+++ Basic Exercises/even_index.py
@@ -1,6 +1,6 @@
 def even_func(text):
     for char in range(0,len(text)-1,2):
-        print("index[char]",text[char])
+        print("index[",char,"]",text[char])
 
 my_string = input("Enter String")
 print("Original string is ", my_string)
