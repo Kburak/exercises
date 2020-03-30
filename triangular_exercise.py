6581b97e4a2fb55e1be3938fd6f88360afa7de40
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-30 17:06:03 +0300

    triangular_exercise , initial commit
    Write Python function triangular(N) that creates a triangular 2D list of integers. You have to fill the item(row,col) of the list with row*col where 1<=row<= N.

---
+++ triangular_exercise.py
@@ -0,0 +1,14 @@
+"""
+Write Python function triangular(N) that creates a triangular 2D list of integers. You have to fill the item(row,col) of the list with row*col where 1<=row<= N.
+"""
+def triangular(N):
+    my_list = []
+    for row in range(1, N+1):
+        list2 = []
+        for col in range(1, row + 1):
+            list2.append(row*col)
+        my_list.append(list2)
+    print(*my_list, sep = "\n")
+
+
+triangular(10)
