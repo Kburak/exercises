8dab05169b3e4bebf4f4242abff3be619a1e2e0a
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-30 23:14:33 +0300

    initial commit for reversed string exercise
    Writing a program (using functions!) that asks the user for a long string containing multiple words.
    Print back to the user the same string, except with the words in backwards order.

---
+++ python-practices/reverse-word-order.py
@@ -0,0 +1,13 @@
+"""
+Write a program (using functions!) that asks the user for a long string containing multiple words.
+Print back to the user the same string, except with the words in backwards order.
+"""
+
+
+def reverse_word(sentence):
+    string_list = sentence.split(" ")
+    return " ".join(string_list[::-1])
+
+user_string = input("Please enter a string and i will give it back to you backwards.")
+
+print(reverse_word(user_string))
