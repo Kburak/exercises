65bd519a4de0f341d094cfc4d11ff555de8c5674
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-03-31 18:20:07 +0300

    updated the indentation and added the __main__

--- python-practices/fibonacci.py
+++ python-practices/fibonacci.py
@@ -1,22 +1,23 @@
 """
-This function gives you the nth fibonacci
+This function gives you the nth fibonacci number
 
 """
-def Fibonacci(n):
 
 
-    if n<=0:
+def Fibonacci_check(n):
+    if n <= 0:
         print("Incorrect input")
     elif n > 300:
         print("This program only goes up to 300")
     # First Fibonacci number is 0
-    elif n==1:
+    elif n == 1:
         return 0
     # Second Fibonacci number is 1
-    elif n==2:
+    elif n == 2:
         return 1
     else:
-        return Fibonacci(n-1)+Fibonacci(n-2)
+        return Fibonacci_check(n - 1) + Fibonacci_check(n - 2)
 
 
-print(Fibonacci(1))
+if __name__ == '__main__':
+    print(Fibonacci_check(10))
