81cba587100aeb09d3cfbe56113b89d62f29cc8b
Kadir Burak Engin <39514824+Kburak@users.noreply.github.com>
2020-04-20 19:10:23 +0300

    initial commit

---
+++ w3resource/math1.py
@@ -0,0 +1,11 @@
+"""
+Write a Python program to solve (x + y) * (x + y)
+"""
+
+def math1(num1,num2):
+    #(num1 + num2) * (num1* num2)
+    # equals to (num1+num2)**2
+    return (num1+num2)**2
+
+print(math1(4,3))
+
